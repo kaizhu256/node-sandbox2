fefb2e349ee83c2878c8714968cac335510f6133
nobody <nobody>
2015-08-12 13:05:04 +0000

    [skip ci]
    2015-08-12T13:05:03Z
    
    copyPwd pwd#master to git@github.com:kaizhu256/sandbox.git#
    
    Linux railsonfire_2f3cf780-2310-0133-4e78-363e62904a12_451b07c93a87 3.13.0-58-generic #97-Ubuntu SMP Wed Jul 8 02:56:15 UTC 2015 x86_64 x86_64 x86_64 GNU/Linux

---
+++ emscripten/tag-1.34.4_64bit_optimizer/CMakeFiles/optimizer.dir/DependInfo.cmake
@@ -0,0 +1,22 @@
+# The set of languages for which implicit dependencies are needed:
+SET(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+SET(CMAKE_DEPENDS_CHECK_CXX
+  "/home/rof/src/github.com/kaizhu256/sandbox/emsdk_portable/emscripten/tag-1.34.4/tools/optimizer/optimizer.cpp" "/home/rof/src/github.com/kaizhu256/sandbox/emsdk_portable/emscripten/tag-1.34.4_64bit_optimizer/CMakeFiles/optimizer.dir/optimizer.cpp.o"
+  "/home/rof/src/github.com/kaizhu256/sandbox/emsdk_portable/emscripten/tag-1.34.4/tools/optimizer/parser.cpp" "/home/rof/src/github.com/kaizhu256/sandbox/emsdk_portable/emscripten/tag-1.34.4_64bit_optimizer/CMakeFiles/optimizer.dir/parser.cpp.o"
+  "/home/rof/src/github.com/kaizhu256/sandbox/emsdk_portable/emscripten/tag-1.34.4/tools/optimizer/simple_ast.cpp" "/home/rof/src/github.com/kaizhu256/sandbox/emsdk_portable/emscripten/tag-1.34.4_64bit_optimizer/CMakeFiles/optimizer.dir/simple_ast.cpp.o"
+  )
+SET(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Targets to which this target links.
+SET(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# The include file search paths:
+SET(CMAKE_C_TARGET_INCLUDE_PATH
+  )
+SET(CMAKE_CXX_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_Fortran_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_ASM_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
