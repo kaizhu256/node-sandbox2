11ce1fa968d71a4d43084b1711f9d56eaa65e4d9
Travis CI <noreply@travis-ci.com>
2015-08-12 11:01:54 +0000

    [skip ci]
    2015-08-12T11:01:53Z
    
    copyPwd pwd#master to git@github.com:kaizhu256/sandbox.git#
    
    Darwin Traviss-Mac-434.local 13.4.0 Darwin Kernel Version 13.4.0: Sun Aug 17 19:50:11 PDT 2014; root:xnu-2422.115.4~1/RELEASE_X86_64 x86_64

---
+++ emscripten/tag-1.34.4/tests/optimizer/simd.js
@@ -0,0 +1,8 @@
+function a(x, y) {
+ x = SIMD_Int32x4_check(x);
+ y = SIMD_Float32x4_check(y);
+ var z = SIMD_Float32x4(0, 0, 0, 0);
+ work(z);
+}
+// EMSCRIPTEN_GENERATED_FUNCTIONS: ["a"]
+
---
+++ emscripten/tag-1.34.4_64bit_optimizer/CMakeFiles/optimizer.dir/DependInfo.cmake
@@ -0,0 +1,22 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/travis/build/kaizhu256/sandbox/emsdk_portable/emscripten/tag-1.34.4/tools/optimizer/optimizer.cpp" "/Users/travis/build/kaizhu256/sandbox/emsdk_portable/emscripten/tag-1.34.4_64bit_optimizer/CMakeFiles/optimizer.dir/optimizer.cpp.o"
+  "/Users/travis/build/kaizhu256/sandbox/emsdk_portable/emscripten/tag-1.34.4/tools/optimizer/parser.cpp" "/Users/travis/build/kaizhu256/sandbox/emsdk_portable/emscripten/tag-1.34.4_64bit_optimizer/CMakeFiles/optimizer.dir/parser.cpp.o"
+  "/Users/travis/build/kaizhu256/sandbox/emsdk_portable/emscripten/tag-1.34.4/tools/optimizer/simple_ast.cpp" "/Users/travis/build/kaizhu256/sandbox/emsdk_portable/emscripten/tag-1.34.4_64bit_optimizer/CMakeFiles/optimizer.dir/simple_ast.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "Clang")
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  )
+set(CMAKE_CXX_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+set(CMAKE_Fortran_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+set(CMAKE_ASM_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
